cb63404788c03c3a508a28dbd3960feed79f44fc
parasyte <parasyte>
2003-09-24 08:17:43 +0000

    release 1.0.2

--- include/config.h
+++ include/config.h
@@ -1,9 +1,20 @@
 /* include/config.h.  Generated automatically by configure.  */
-/* include/config.h.in.  Generated automatically from configure.in by autoheader.  */
+/* include/config.h.in.  Generated automatically from configure.ac by autoheader.  */
+
+/* Define if you have the <curses/curses.h> header file. */
+#define HAVE_CURSES_CURSES_H 1
+
+/* Define if you have the <curses.h> header file. */
+#define HAVE_CURSES_H 1
+
+/* Define if you have the <termios.h> header file. */
+#define HAVE_TERMIOS_H 1
 
 /* Name of package */
 #define PACKAGE "run-free"
 
-/* Version number of package */
-#define VERSION "1.0.0"
+/* Define if you have the ANSI C header files. */
+#define STDC_HEADERS 1
 
+/* Version number of package */
+#define VERSION "1.0.2"
